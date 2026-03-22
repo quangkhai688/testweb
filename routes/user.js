e725720abf23d108eb4f9deff9198d09e8d4a4fa
quangkhai688 <q.khai688@gmail.com>
2026-03-23 03:06:58 +0700

    Update database import path in user routes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt  = require('bcryptjs');
-const db      = require('../db');
+const db      = require('../src/config/db')
 const { authenticate } = require('../middleware/auth');
 
 const router = express.Router();
